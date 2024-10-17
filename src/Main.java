a1acbbcfa150eedfab95594319b9c0bb4ac28587
Ilyas Miftakhov <imiftakhov@devexperts.com>
2024-10-17 22:02:28 +0200

    init commit.
    Main.java has been added

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello chort 48.2!");
+        System.out.println("Welcome to git practice!");
+    }
+}
